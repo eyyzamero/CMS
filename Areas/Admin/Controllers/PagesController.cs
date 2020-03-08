08619d0b0d6330335d9e3753c744d6f8294d4b73
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-08 15:21:54 +0100

    Initial PagesController and Pages HTML config, fetching data from DB

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -1,13 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using CMS.Models.Data;
+using CMS.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CMS.Areas.Admin.Controllers
 {
-    public class PagesController : Controller
-    {
-        // GET: Admin/Pages
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class PagesController : Controller
+	{
+		// GET: Admin/Pages
+		public ActionResult Index()
+		{
+			// Declaring list of PageVM
+			List<PageVM> PagesList;
+
+			using (DB db = new DB())
+			{
+				// Initializing list
+				PagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+			}
+
+			// Return pages to view
+
+			return View(PagesList);
+		}
+	}
 }
