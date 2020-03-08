30518edbd7bd98ffa817e0d8e2681162dd82e9d9
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-08 16:12:08 +0100

    AddPage View and Controller completed

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -18,12 +18,64 @@ namespace CMS.Areas.Admin.Controllers
 			{
 				// Initializing list
 				PagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
-
 			}
 
 			// Return pages to view
-
 			return View(PagesList);
+		}
+
+		// GET Admin/Pages/AddPage
+		[HttpGet]
+		public ActionResult AddPage() {
+
+			return View();
+		}
+
+		// POST Admin/Pages/AddPage
+		[HttpPost]
+		public ActionResult AddPage(PageVM model)
+		{
+			// Check whether form is valid
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			using (DB db = new DB())
+			{
+				string slug;
+				// Initializing PageDTO
+				PageDTO dto = new PageDTO();
+
+				// When page URL is undefined Page title is assigned to Slug
+				if (string.IsNullOrWhiteSpace(model.Slug))
+				{
+					slug = model.Title.Replace(" ", "-").ToLower();
+				} else
+				{
+					slug = model.Slug.Replace(" ", "-").ToLower();
+				}
+
+				// Prevent adding same page
+				if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+				{
+					ModelState.AddModelError("", "Your Page Title or Page URL is already taken");
+					return View(model);
+				}
+
+				dto.Title = model.Title;
+				dto.Slug = slug;
+				dto.Body = model.Body;
+				dto.Sorting = 1000;
+				dto.HasSidebar = model.HasSidebar;
+
+				// Saving DTO
+				db.Pages.Add(dto);
+				db.SaveChanges();
+			}
+			TempData["SM"] = "You have added a new Page!";
+
+			return RedirectToAction("AddPage");
 		}
 	}
 }
--- Models/ViewModels/Pages/PageVM.cs
+++ Models/ViewModels/Pages/PageVM.cs
@@ -23,12 +23,16 @@ namespace CMS.Models.ViewModels.Pages
         public int Id { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 3)]
+        [Display(Name = "Page Title")]
         public string Title { get; set; }
+        [Display(Name = "Page URL")]
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [Display(Name = "Page Content")]
         public string Body { get; set; }
         public int Sorting { get; set; }
+        [Display(Name = "Does it have sidebar?")]
         public bool HasSidebar { get; set; }
     }
 }
