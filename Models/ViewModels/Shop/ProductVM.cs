3c55718065703739e62706d51d0cecab9fb6ec55
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-09 15:47:54 +0100

    ProductVM and ProductDTO init

---
+++ Models/ViewModels/Shop/ProductVM.cs
@@ -0,0 +1,42 @@
+﻿using CMS.Models.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CMS.Models.ViewModels.Shop
+{
+    public class ProductVM
+    {
+        ProductVM()
+        {
+
+        }
+
+        ProductVM(ProductDTO row)
+        {
+            Id = row.Id;
+            Name = row.Name;
+            Slug = row.Slug;
+            Description = row.Description;
+            Price = row.Price;
+            CategoryName = row.CategoryName;
+            CategoryId = row.CategoryId;
+            ImageName = row.ImageName;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; }
+        [Required]
+        public string CategoryId { get; set; }
+        public string ImageName { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<string> GalleryImages { get; set; }
+    }
+}
