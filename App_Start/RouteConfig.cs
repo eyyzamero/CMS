30d6e5bd80fed4f045783c1e05a72503bb6884e2
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-11 17:46:14 +0100

    Partial View for Pages Routing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace CMS
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute("PageMenuPartial", "Pages/PageMenuPartial", new { controller = "Pages", action = "PageMenuPartial" }, new[] { "CMS.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "CMS.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "CMS.Controllers" });
+
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
---
+++ Controllers/PagesController.cs
@@ -0,0 +1,60 @@
+﻿using CMS.Models.Data;
+using CMS.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            // Setting URL
+            if (page == "") page = "home";
+
+            // Declaring PageVM and PageDTO
+            PageVM model;
+            PageDTO dto;
+
+            // Checking whether the site does exist
+            using (DB db = new DB())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page))) return RedirectToAction("Index", new { page = "" });
+            }
+
+            // Fetching pageDTO
+            using (DB db = new DB())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // Updating page Title
+            ViewBag.PageTitle = dto.Title;
+
+            // Check whether the site has sidebar
+            if (dto.HasSidebar == true) ViewBag.Sidebar = true;
+             else ViewBag.Sidebar = false;
+
+            // PageVM init
+            model = new PageVM(dto);
+
+            return View(model);
+        }
+
+        public ActionResult PageMenuPartial() {
+            // Declaring PageVM
+            List<PageVM> pageVMList;
+
+            // Fetching pages
+            using (DB db = new DB())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting).Where(x => x.Slug != "home").Select(x => new PageVM(x)).ToList();
+            }
+            return PartialView(pageVMList);
+        }
+    }
+}
