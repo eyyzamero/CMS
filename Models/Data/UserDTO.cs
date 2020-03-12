484276ea62ec563d576628b77525580d4043fef2
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-12 13:37:23 +0100

    Generated bunch of DTO's related to User and Roles - preparing for Auth

---
+++ Models/Data/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMS.Models.Data
+{
+    [Table("Users")]
+    public class UserDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/ViewModels/Account/UserVM.cs
@@ -0,0 +1,39 @@
+﻿using CMS.Models.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models.ViewModels.Account
+{
+    public class UserVM
+    {
+        public UserVM()
+        {
+
+        }
+
+        public UserVM(UserDTO row)
+        {
+            Id = row.Id;
+            FirstName = row.FirstName;
+            LastName = row.LastName;
+            EmailAdress = row.EmailAdress;
+            UserName = row.UserName;
+            Password = row.Password;
+        }
+
+
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAdress { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
