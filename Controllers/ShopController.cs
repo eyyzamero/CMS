9cdffe58ea4e5fbdd9b4b7abc22d790de3f2c5d9
eyyzam <bronislawskiandrzej@gmail.com>
2020-03-12 01:01:00 +0100

    Built Product Details View and added funcionality to it

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,6 +1,7 @@
 ﻿using CMS.Models.Data;
 using CMS.Models.ViewModels.Shop;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -26,6 +27,54 @@ namespace CMS.Controllers
             }
 
             return PartialView(categoryVMList);
+        }
+
+        public ActionResult Category(string name)
+        {
+            // Declaring productVMList
+            List<ProductVM> productVMList;
+            using (DB db = new DB())
+            {
+                // Fetch ID of category
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = categoryDTO.Id;
+
+                // Initalizing list of products
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                // Fetching name of category
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+                ViewBag.CategoryName = productCat.CategoryName;
+            }
+            return View(productVMList);
+        }
+
+        [ActionName("Product-Details")]
+        public ActionResult ProductDetails(string name)
+        {
+            // ProductVM and ProductDTO declaration
+            ProductVM model;
+            ProductDTO dto;
+
+            // Initalizing productID
+            int id = 0;
+
+            using (DB db = new DB()) {
+                // Checking whether the product does exist
+                if (!db.Products.Any(x => x.Slug.Equals(name))) return RedirectToAction("Index", "Shop");
+
+                // Initalizing productDTO
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                // Fetching ID
+                id = dto.Id;
+
+                // Initalizing model
+                model = new ProductVM(dto);
+            }
+            // Getting galery of images for selected product
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs")).Select(fn => Path.GetFileName(fn));
+            return View("ProductDetails", model);
         }
     }
 }
